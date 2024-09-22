37d07f9b063a80bee3f70cbdbc2b0bcfcc337760
Pedro Brasca <pedropaulobrasca@gmail.com>
2024-09-22 20:52:26 -0300

    Add social links

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Moon, Sun, Download, Languages } from 'lucide-react';
+import { Moon, Sun, Download, Languages, Linkedin, Github } from 'lucide-react';
 
 interface HeaderProps {
   isDarkMode: boolean;
@@ -18,16 +18,26 @@ export default function Header({
   buttonStyle,
 }: HeaderProps) {
   return (
-    <div className="flex justify-end space-x-4 mb-4">
-      <button onClick={toggleDarkMode} className={buttonStyle}>
-        {isDarkMode ? <Sun className="h-6 w-6" /> : <Moon className="h-6 w-6" />}
-      </button>
-      <button onClick={toggleLanguage} className={buttonStyle}>
-        <Languages className="h-6 w-6" />
-      </button>
-      <button onClick={exportToPDF} className={buttonStyle}>
-        <Download className="h-6 w-6" />
-      </button>
+    <div className="flex justify-between space-x-4 mb-4">
+      <div className='flex gap-4'>
+        <a href='https://www.linkedin.com/in/pedro-paulo-brasca-trevisanuto-86136114a/' className={buttonStyle} target='_blank'>
+          <Linkedin className="h-6 w-6" />
+        </a>
+        <a href='https://github.com/pedropaulobrasca' className={buttonStyle} target='_blank'>
+          <Github className="h-6 w-6" />
+        </a>
+      </div>
+      <div className='flex gap-4'>
+        <button onClick={toggleDarkMode} className={buttonStyle}>
+          {isDarkMode ? <Sun className="h-6 w-6" /> : <Moon className="h-6 w-6" />}
+        </button>
+        <button onClick={toggleLanguage} className={buttonStyle}>
+          <Languages className="h-6 w-6" />
+        </button>
+        <button onClick={exportToPDF} className={buttonStyle}>
+          <Download className="h-6 w-6" />
+        </button>
+      </div>
     </div>
   );
 }
